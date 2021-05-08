1ae02ccd8c8fe8ff799ffd13a3e7c686d9b55c0a
marm0nt <81480193+marm0nt@users.noreply.github.com>
2021-05-08 18:27:51 -0500

    Add files via upload

---
+++ TimeConversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Timeconversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the total amount of time in minutes only");
+            int totalTimeInMinutes = int.Parse(Console.ReadLine());
+            int hours = totalTimeInMinutes / 60;
+            int remainingMinutes = totalTimeInMinutes % 60;
+
+
+            Console.WriteLine("{0} minutes is equal to {1} hours and {2} minutes", totalTimeInMinutes,hours,remainingMinutes);
+
+
+
+
+        }
+    }
+}
