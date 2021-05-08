692537ce44f1f9b2d4d6d3eb72b8e98ed1084b45
marm0nt <81480193+marm0nt@users.noreply.github.com>
2021-05-08 17:54:55 -0500

    Add files via upload
    A mini-application that asks the user for their room measurements. It calculates the cost based on the answer and price per square ft constant.

---
+++ HomeDepotSimulationCalc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Pb4Lecture._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int per_square_foot = 5;
+
+            
+            Console.WriteLine("What is the length of your room?");
+            int length = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is the width of your room?");
+            int width = int.Parse(Console.ReadLine());
+
+            int area = length * width;
+            int price = area * per_square_foot;
+
+
+
+
+            Console.WriteLine("Awesome the price of your {0} by {1} is ${2}", length, width, price);
+
+           
+           
+
+
+
+
+            
+            
+            
+
+
+        }
+    }
+}
